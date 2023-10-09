3ca87351465b669f8d62ad9515c1aabddf18779e
Oksana Pavlova <77125973+Oksana-max@users.noreply.github.com>
2023-10-09 15:37:00 +0300

    Delete js directory

--- js/index.js
+++
@@ -1,9 +0,0 @@
-const cardSale = document.querySelectorAll(".item");
-
-cardSale.forEach((el) => {
-  el.addEventListener("click", (e) => {
-    if (e.target.className === "item__arrow") {
-      e.target.parentNode.querySelector(".item__select-wrapper").classList.toggle("openCard");
-    }
-  });
-});
